4c7263d9212968a526c47342f60dc1d1afea9e91
Abraham <anubra266@gmail.com>
2021-07-05 01:09:45 +0100

    Add Chakra UI

--- src/extension.ts
+++ src/extension.ts
@@ -1,94 +1,67 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 export function activate(context: vscode.ExtensionContext) {
-	/// Activate flutter build
-	const buildFlutter = vscode.commands.registerCommand('flutter-touchbar-helper.flutterbuild', () => {
-		vscode.commands.executeCommand('setContext', 'enableBuild', true);
-	});
+  const git = vscode.commands.registerCommand("choc-touchbar.git", () => {
+    vscode.commands.executeCommand("setContext", "enableGit", true);
+  });
 
-	/// Closing build options
-	const closeBuild = vscode.commands.registerCommand('flutter-touchbar-helper.closebuild', () => {
-		vscode.commands.executeCommand('setContext', 'enableBuild', false);
-	});
+  // Git commit
+  const commit = vscode.commands.registerCommand(
+    "choc-touchbar.gitcommit",
+    () => {
+      vscode.commands.executeCommand("git.commit");
+      vscode.commands.executeCommand("setContext", "enableGit", false);
+    }
+  );
 
-	/// Closing git
-	const closeGit = vscode.commands.registerCommand('flutter-touchbar-helper.closegit', () => {
-		vscode.commands.executeCommand('setContext', 'enableGit', false);
-	});
+  /// Git push
+  const push = vscode.commands.registerCommand("choc-touchbar.gitpush", () => {
+    vscode.commands.executeCommand("git.push");
+    vscode.commands.executeCommand("setContext", "enableGit", false);
+  });
 
-	/// Close tools
-	const closeTools = vscode.commands.registerCommand('flutter-touchbar-helper.closetools', () => {
-		vscode.commands.executeCommand('setContext', 'enableTools', false);
-	});
+  /// Closing git
+  const closeGit = vscode.commands.registerCommand(
+    "choc-touchbar.closegit",
+    () => {
+      vscode.commands.executeCommand("setContext", "enableGit", false);
+    }
+  );
 
-	/// Open tools	
-	const openTools = vscode.commands.registerCommand('flutter-touchbar-helper.fluttertools', () => {
-		vscode.commands.executeCommand('setContext', 'enableTools', true);
-	});
-	
-	const git = vscode.commands.registerCommand('flutter-touchbar-helper.git', () => {
-		vscode.commands.executeCommand('setContext', 'enableGit', true);
-	});
+  const chakra = vscode.commands.registerCommand("choc-touchbar.chakra", () => {
+    vscode.commands.executeCommand("setContext", "enableChakra", true);
+  });
 
-	// Git commit 
-	const commit = vscode.commands.registerCommand('flutter-touchbar-helper.gitcommit', () => {
-		vscode.commands.executeCommand('git.commit');
-		vscode.commands.executeCommand('setContext', 'enableGit', false);
-	});
+  /// Chakra docs
+  const chakraDocs = vscode.commands.registerCommand(
+    "choc-touchbar.chakradocs",
+    () => {
+      vscode.env.openExternal(vscode.Uri.parse("https://chakra-ui.com"));
+      //   vscode.commands.executeCommand("setContext", "enableChakra", false);
+    }
+  );
 
-	/// Git push
-	const push = vscode.commands.registerCommand('flutter-touchbar-helper.gitpush', () => {
-		vscode.commands.executeCommand('git.push');
-		vscode.commands.executeCommand('setContext', 'enableGit', false);
-	});
+  /// Closing chakra
+  const closeChakra = vscode.commands.registerCommand(
+    "choc-touchbar.closechakra",
+    () => {
+      vscode.commands.executeCommand("setContext", "enableChakra", false);
+    }
+  );
 
-	/// Activate build single apk
-	const buildSingleApk = vscode.commands.registerCommand('flutter-touchbar-helper.build.singleapk', () => {
-		vscode.commands.executeCommand('setContext', 'enableBuild', false);
-		const terminal = vscode.window.createTerminal('Flutter Build APK');
-		terminal.show();
-		terminal.sendText("flutter clean");
-		terminal.sendText("flutter build apk --release");
-	});
-
-	/// Activate build single appbundle
-	const buildSingleAppbundle = vscode.commands.registerCommand('flutter-touchbar-helper.build.singleappbundle', () => {
-		vscode.commands.executeCommand('setContext', 'enableBuild', false);
-		const terminal = vscode.window.createTerminal('Flutter Build Appbundle');
-		terminal.show();
-		terminal.sendText("flutter clean");
-		terminal.sendText("flutter build appbundle --release");
-	});
-
-	/// Activate build split per abi
-	const buildSplitABI = vscode.commands.registerCommand('flutter-touchbar-helper.build.splitabi', () => {
-		vscode.commands.executeCommand('setContext', 'enableBuild', false);
-		const terminal = vscode.window.createTerminal('Flutter Build ABI');
-		terminal.show();
-		terminal.sendText("flutter clean");
-		terminal.sendText("flutter build apk --split-per-abi --release");
-	});
-
-	/// Activate flutter clean
-	const cleanFlutter = vscode.commands.registerCommand('flutter-touchbar-helper.flutterclean', () => {
-		const terminal = vscode.window.createTerminal('Flutter Clean Terminal');
-		terminal.show();
-		terminal.sendText("flutter clean");
-	});	
-
-	/// Activate devtools
-	const devtools = vscode.commands.registerCommand('flutter-touchbar-helper.devtools', () => {
-		const terminal = vscode.window.createTerminal('Flutter DevTools Terminal');
-		terminal.show();
-		terminal.sendText("flutter pub global run devtools");
-
-	});	
-
-	context.subscriptions.push(cleanFlutter, buildFlutter, buildSingleApk, buildSingleAppbundle, buildSplitABI, closeBuild, git, closeGit, commit, push, openTools, closeTools, devtools);
+  context.subscriptions.push(
+    git,
+    closeGit,
+    commit,
+    push,
+    chakra,
+    chakraDocs,
+    closeChakra
+  );
 }
 
 // this method is called when your extension is deactivated
